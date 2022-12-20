fdb02893a358761d88968a4b347eb26408ec90cb
damien <damien@damienjburks.com>
2022-12-20 00:28:53 +0000

    finalizing changes for terraform

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
-# AWSome-Honey-Pot
-Terraform scripts that create a honey pot within AWS
+# AWSome Honey Pot
+
+Hello! Welcome to the code that I've written called the AWSome Honey Pot. The purpose of the repository is to house Terraform templates and files that are used to create a HoneyPot within AWS using [T-Pot](https://github.com/telekom-security/tpotce).
+
+I've received a lot of my guidance on how to create this from the following articles:
+
+- [Creating a Research Honeypot on AWS](https://blog.devgenius.io/creating-a-research-honeypot-on-aws-b0ded134729a)
+
+It is a fantastic read, and I do encourage you all to read this article.
+
+---
+
+## Prerequisities
+
+In order to execute deploy this infrastructure, you'll need to ensure you have the following installed:
+
+1. Subscribe to [Debian Buster AMI](https://aws.amazon.com/marketplace/pp/prodview-vh2uh3o4pdfow)<span style="color:red"> - **ABSOLUTE REQUIREMENT!** </span>
+2. Terraform v1.3+
+3. AWS CLI
+    >**NOTE** You'll need to have this installed so you can configure your aws profile prior to deployment.
+
+For more information about installing the necessary technologies, feel free to check out these resources:
+- [Terraform Installation Guide](https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli).
+- [Installing AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html)
+
+---
+
+## Deployment to AWS
+
+Assuming you have your AWS CLI configured properly for your account, you'll need to execute the following commands:
+
+1. Modify the `main.tf` file inside of the workspace folder and insert your a list of IP addresses that you would like to access the administration panel and SSH into the machine.
+
+    ```terraform
+    resource "aws_security_group" "tpot_security_group" {
+        ...
+
+        ingress {
+            from_port   = 64294
+            to_port     = 64294
+            protocol    = "tcp"
+            cidr_blocks = ["your_ip_addr_cidr"] # Change your IP address here
+        }
+        ingress {
+            from_port   = 64295
+            to_port     = 64295
+            protocol    = "tcp"
+            cidr_blocks = ["your_ip_addr_cidr"] # Change to your IP address here
+        }
+        ingress {
+            from_port   = 64297
+            to_port     = 64297
+            protocol    = "tcp"
+            cidr_blocks = ["your_ip_addr_cidr"] # Change to your IP address here
+        }
+
+        ...
+    }
+    ```
+
+2. Initialize Terraform
+    ```bash
+    $ terraform init
+    ```
+
+3. Apply the entire plan to the AWS account
+    ```bash
+    $ terraform apply -auto-approve
+    ```
+    >**NOTE**: This will take between 15-30 minutes. Feel free to step away while this is working. It is deploying your infrastructure to your account.
+
+    Once the plan has been created and applied, a private key should be created within the workspace folder called `hp_key_pair.pem`. You can use this key to ssh into the instance.
+
+4. Verify that everything is up.
+    
+    1. Log into the admin portal (https://<your_instance_ip>:64297) using the credentials listed below:
+    ```
+    username: webuser
+    password: w3b$ecret
+    ```
+
+    After logging in, you should be redirected to this page:
+    #### Image 1: T-Pot Homepage (Admin portal)
+    ![Homepage](./images/tpot_homepage.png)
+    If not, check your security groups and ensure you have whitelisted the right IP address CIDRs.
+
+    >**NOTE**: The default credentials can be changed at anytime inside of the web portal, which I highly recommend you do change them.
+
+---
+
+## Cleanup Resources
+
+If at any point you want to clean up your resources, please feel free to run the following command:
+
+```bash
+$ terraform destroy -auto-approve
+```
+
+This will destroy all of the resources that were spun up in the account. 
+
+Otherwise, happy hacking!! :smile!
--- workspace/main.tf
+++ workspace/main.tf
@@ -1,20 +1,40 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-}
-
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 # Network Dependencies
 resource "aws_vpc" "honeypot_vpc" {
-    cidr_block = "192.168.1.0/24"
+  cidr_block           = "192.168.1.0/24"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "TPOT_VPC"
+  }
 }
+
+resource "aws_internet_gateway" "tpot_vpc_igw" {
+  vpc_id = aws_vpc.honeypot_vpc.id
+  tags = {
+    Name = "TPOT_VPC_IGW"
+  }
+}
+
+resource "aws_route_table" "tpot_route_table" {
+  vpc_id = aws_vpc.honeypot_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tpot_vpc_igw.id
+  }
+  tags = {
+    Name = "TPOT Default Route Table"
+  }
+}
+
+resource "aws_route_table_association" "tpot_rt_association" {
+  subnet_id      = aws_subnet.hp_instance_subnet.id
+  route_table_id = aws_route_table.tpot_route_table.id
+}
+
 
 resource "aws_subnet" "hp_instance_subnet" {
   vpc_id            = aws_vpc.honeypot_vpc.id
@@ -26,68 +46,89 @@ resource "aws_subnet" "hp_instance_subnet" {
   }
 }
 
-resource "aws_security_group" "default_hp_sg" {
-  name        = "hp_sg"
-  description = "Default HP SG for instance"
+resource "aws_security_group" "tpot_security_group" {
+  name        = "T-Pot SG"
+  description = "T-Pot Honeypot SG"
   vpc_id      = aws_vpc.honeypot_vpc.id
 
+  ingress { # AWS System Logs Debugging
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.honeypot_vpc.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.honeypot_vpc.ipv6_cidr_block]
+    from_port   = 0
+    to_port     = 64000
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 64294
+    to_port     = 64294
+    protocol    = "tcp"
+    cidr_blocks = ["your_ip_addr_cidr"] # Change your IP address here
+  }
+  ingress {
+    from_port   = 64295
+    to_port     = 64295
+    protocol    = "tcp"
+    cidr_blocks = ["your_ip_addr_cidr"] # Change to your IP address here
+  }
+  ingress {
+    from_port   = 64297
+    to_port     = 64297
+    protocol    = "tcp"
+    cidr_blocks = ["your_ip_addr_cidr"] # Change to your IP address here
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "honeypot_security_group"
+    Name = "T-Pot SG"
   }
 }
 
-resource "aws_network_interface" "nw_int" {
-  subnet_id   = aws_subnet.hp_instance_subnet.id
-  private_ips = ["192.168.1.4"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
+resource "aws_eip" "tpot_instance_eip" {
+  instance = aws_instance.tpot_instance.id
+  vpc      = true
 }
 
 # Main HoneyPot Instance
-resource "aws_instance" "honeypot_instance" {
-  ami           = "ami-06e9b30b57b32ec3a"
-  instance_type = "t2.Xlarge"
-  subnet_id = aws_subnet.hp_instance_subnet.id
-  
-  network_interface {
-    network_interface_id = aws_network_interface.nw_int.id
-    device_index = 0
-  }
+resource "aws_instance" "tpot_instance" {
+  ami           = "ami-0a6cdd2c74bcdc701"
+  instance_type = "t2.large"
+  subnet_id     = aws_subnet.hp_instance_subnet.id
+  key_name      = aws_key_pair.tf_key_pair.key_name
 
-  security_groups = [aws_security_group.default_hp_sg.id]
-
-  ebs_block_device = {
-    volume_type = "gp2"
-    volume_size = "128"
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 128
     delete_on_termination = true
   }
 
+  vpc_security_group_ids      = [aws_security_group.tpot_security_group.id]
+  associate_public_ip_address = true
+
   tags = {
-    Name = "HoneyPot Management Instance"
+    Name = "T-Pot Honeypot Instance"
   }
 }
 
 # Instance Key Pair Generation
-resource "aws_key_pair" "tf-key-pair" {
-    key_name = "tf-key-pair"
-    public_key = tls_private_key.rsa.public_key_openssh
+resource "aws_key_pair" "tf_key_pair" {
+  key_name   = "hp_key_pair"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 resource "tls_private_key" "rsa" {
-    algorithm = "RSA"
-    rsa_bits  = 4096
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
-resource "local_file" "tf-key" {
-    content  = tls_private_key.rsa.private_key_pem
-    filename = "honeypot-instance-key"
+resource "local_file" "tf_key" {
+  content  = tls_private_key.rsa.private_key_openssh
+  filename = "hp_key_pair.pem"
 }
